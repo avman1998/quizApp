fabcaa617dde37ccba17b2b2e2ea4949c6f0790b
Aman <avman1998@gmail.com>
2022-11-20 23:09:20 +0530

    [burger updated]

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -36,7 +36,7 @@ export default function Intro(){
    
     return(
         <div className="bg-gray-100 min-h-[100vh]">
-      <div className="bg-black flex justify-between items-center min-w-[100vw] min-h-[6vh] md:min-h-[8vh]"><Burger/>{user?.displayName && <p className="text-white p-[10px]">{user?.displayName}</p>}</div>
+      <div className="bg-black flex justify-between items-center min-w-[100vw] min-h-[8vh] md:min-h-[8vh]"><Burger/>{user?.displayName && <p className="text-white p-[10px]">{user?.displayName}</p>}</div>
        <ToastContainer theme="light" position="bottom-right" />
         {/* <div class="flex flex-row  items-center justify-center gap-[10px] absolute right-[5px] top-[5px] text-[90%] ">
         <p className="text-black font-semibold text-[80%]">{user?.displayName}</p>
@@ -45,7 +45,7 @@ export default function Intro(){
         </div> */}
         {/* <button onClick={notify}>Notify!</button> */}
      
-        <div className="intro min-h-[80vh] min-w-[100wh]  text-black gap-[20px] md:gap-[30px] flex flex-col items-center justify-start px-[20px] pt-[80px] md:pt-[30px] pb-[0px]">
+        <div className="intro min-h-[92vh] md:min-h-[92vh] min-w-[100wh]  text-black gap-[20px] md:gap-[30px] flex flex-col items-center justify-start px-[20px] pt-[80px] md:pt-[30px] pb-[0px]">
             <h1 className="md:text-5xl text-[165%] font-semibold text-center">Welcome to Quizoo!</h1>
             <p className="text-center italic mt-[20px] mb-[80px]">“I love anything quiz-related.”  <br/>~ Natasha Hamilton</p>
           
