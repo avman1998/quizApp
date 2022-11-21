51d9cb8ed956256812bf39fc1fc7de27b934766f
Aman <avman1998@gmail.com>
2022-11-21 14:35:27 +0530

    update

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase";
-import { useAuth } from "../../AuthContext";
-export default function History() {
-  const { user } = useAuth();
-  return <>summary</>;
-}
+// import React from "react";
+// import { doc, getDoc } from "firebase/firestore";
+// import { db } from "../../firebase";
+// import { useAuth } from "../../AuthContext";
+// export default function History() {
+//   const { user } = useAuth();
+//   return <>summary</>;
+// }
